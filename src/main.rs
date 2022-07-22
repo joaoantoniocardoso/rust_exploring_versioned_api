e9926a39555c2b096bc496e0ef8f16a62ff727ac
João Antônio Cardoso <joao.maker@gmail.com>
2022-07-22 18:13:26 -0300

    Use internally tag works, quite odd.

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,58 @@
-
-use serde_json::json;
 use obake;
+use serde_json::json;
 
 #[obake::versioned]
 #[obake(version("0.1.0"))]
 #[obake(version("0.2.0"))]
 #[obake(version("0.3.0"))]
 #[obake(derive(serde::Serialize, serde::Deserialize))]
-#[obake(serde(untagged))]
+#[obake(serde(tag = "version"))]
 #[derive(Debug, Default, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
 struct Character {
+    #[obake(cfg(">=0.1.0"))]
     name: String,
     age: u32,
+    #[serde(default)]
     #[obake(cfg(">=0.2.0"))]
     height: f32,
+    #[serde(default)]
     #[obake(cfg(">=0.3.0"))]
     weight: f32,
 }
 
 impl From<Character!["0.1.0"]> for Character!["0.2.0"] {
     fn from(old: Character!["0.1.0"]) -> Self {
-        Self {
-            name: old.name,
-            age: old.age,
-            ..Default::default()
-        }
+        let serialised = serde_json::to_string(&old).unwrap();
+        serde_json::from_str(&serialised).unwrap()
     }
 }
 
 impl From<Character!["0.2.0"]> for Character!["0.3.0"] {
     fn from(old: Character!["0.2.0"]) -> Self {
-        Self {
-            name: old.name,
-            age: old.age,
-            height: old.height,
-            ..Default::default()
-        }
+        let serialised = serde_json::to_string(&old).unwrap();
+        serde_json::from_str(&serialised).unwrap()
     }
 }
 
-fn main() {}
+fn main() {
+    let character: VersionedCharacter = Character::default().into();
+    dbg!(serde_json::to_string_pretty(&character));
+}
 
 #[cfg(test)]
 mod tests {
-    use serde_json::json;
     use crate::*;
+    use serde_json::json;
 
     #[test]
     fn from_2_to_3() {
         let freeza_ser = serde_json::to_string_pretty(&json!({
+            "version": "Character_v0_2_0",
             "name": "Freeza",
             "age": 32,
             "height": 1.53,
-        })).unwrap();
+        }))
+        .unwrap();
         dbg!(&freeza_ser);
 
         let freeza: VersionedCharacter = serde_json::from_str(&freeza_ser).unwrap();
@@ -71,9 +71,11 @@ mod tests {
     #[test]
     fn from_1_to_2_to_3() {
         let goku_ser = serde_json::to_string_pretty(&json!({
+            "version": "Character_v0_1_0",
             "name": "Goku",
             "age": 42,
-        })).unwrap();
+        }))
+        .unwrap();
         dbg!(&goku_ser);
 
         let goku: VersionedCharacter = serde_json::from_str(&goku_ser).unwrap();
@@ -88,5 +90,4 @@ mod tests {
         };
         assert_eq!(goku_expected, goku)
     }
-
 }
